4ae6cd3649c04be55a130f5997b72eee48794c98
Ko sungbin <kosb7544@gmail.com>
2024-11-18 19:06:58 +0900

    add how repository works

--- src/main/java/com/springboot/chap3/TestService.java
+++ src/main/java/com/springboot/chap3/TestService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TestService {
@@ -12,5 +13,14 @@ public class TestService {
 
     public List<Member> getAllMembers() {
         return memberRepository.findAll();
+    }
+
+    public void jpaTest() {
+        memberRepository.save(new Member(1L, "A"));
+
+        Optional<Member> member = memberRepository.findById(1L);
+        List<Member> allmembers = memberRepository.findAll();
+
+        memberRepository.deleteById(1L);
     }
 }
